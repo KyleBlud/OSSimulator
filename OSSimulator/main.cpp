8ed3e2d200e95b2d0bbe4eacc3f7bcde229f8c67
KyleBlud <kylebludworth@outlook.com>
2016-09-18 18:57:34 -0400

    Part 1
    Added Part 1 of project

---
+++ OSSimulator/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  OSSimulator
+//
+//  Created by Kyle Bludworth on 8/25/16.
+//  Copyright © 2016 Kyle Bludworth. All rights reserved.
+//
+
+#include <iostream>
+#include "Interface.hpp"
+
+int main(int argc, const char * argv[]) {
+    
+    Interface gui;
+        
+    return 0;
+}
